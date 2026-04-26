b2a0a944f66b60cc35d30ffa85ef3990141a4aff
devmatheus <dev.matheuslopes@hotmail.com>
2026-04-26 12:02:33 -0300

    Implementando a função de adicionar tarefas.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var inputTarefas = document.getElementById('inputTarefa');
+var lista = document.querySelector('#lista ul');
 
 var listaTarefas = [];
 
@@ -6,12 +7,31 @@ function adicionar() {
     if(inputTarefas.value === "" || inputTarefas === null){
         alert("Digite uma tarefa!!!")
         inputTarefas.value = "";
-    }else{
-        listaTarefas.push(inputTarefas.value);
+    } else {
+        let tarefa = inputTarefas.value;
+        listaTarefas.push(tarefa);
+        inputTarefas.value = "";
+        rederLista();
     }
-    console.log(listaTarefas);
+
 }
 
 function rederLista() {
-    
+    lista.innerHTML = "";
+    listaTarefas.map((item)=>{
+        
+        let itemLista= document.createElement("li");
+        itemLista.innerHTML = item;
+        lista.appendChild(itemLista);
+
+        let bntexcluir = document.createElement("button");
+        bntexcluir.innerText = "Excluir";
+        bntexcluir.onclick = "excluirTarefa()";
+        itemLista.appendChild(bntexcluir);
+
+    })
 }
+
+// function excluirTarefa() {
+//     console.log("Excluido");
+// }
